c3770b123ed00d3cf7c1bfa436516f15bf1acad2
Shin Dain <codedain@gmail.com>
2023-06-18 16:29:19 +0900

    02_01_보간법 및 데이터 바인딩
    {{}}과 data property

--- 01_start/app.js
+++ 01_start/app.js
@@ -1,19 +1,20 @@
 Vue.createApp({
-  // data: function(){}
+    // data: function(){}
 
-  data() {
-    //객체를 반환해야함
-    return {
-      goals: [],
-      enteredValue: '',
-    };
-  },
-
-  methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      //입력란을 지우고 싶다면
-      this.enteredValue = '';
+    data() {
+        //객체를 반환해야함
+        return {
+            goals: [],
+            enteredValue: '',
+        };
     },
-  },
+
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            //입력란을 지우고 싶다면
+            this.enteredValue = '';
+            git;
+        },
+    },
 }).mount('#app');
---
+++ 02_vue를 이용한 dom과 상호작용/01_보간법 및 데이터 바인딩/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    // app객체의 property로 data는 key, value로는 함수를 갖는다.
+    // data는 그 자체로 함수이자 메서드
+    // data: function(){}
+    //  단축가능
+    data() {
+        //항상 객체를 반환한다.
+        return {
+            courseGoal: 'Finish the course and learn Vue!',
+        };
+    },
+});
+
+app.mount('#user-goal');
