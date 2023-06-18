725e611bd38ec66a54236e1122083f7f45a135e9
Shin Dain <codedain@gmail.com>
2023-06-18 19:09:08 +0900

    03_04_네이티브 이벤트 객체 사용하기
    v-on:input=""으로 사용자 입력
    함수의 매개변수로 event객체가 들어온다.
    사용자 입력값은 event.target.value

--- 03_이벤트 바인딩/app.js
+++ 03_이벤트 바인딩/app.js
@@ -2,9 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             counter: 0,
+            name: '',
         };
     },
     methods: {
+        setName(event, lastName) {
+            // 이벤트가 발생할 때 실행될 함수를 가리키면
+            // 그 함수는 브라우저가 제공하는 인수를 얻게 된다 = event
+            // 이 이벤트 객체는 이벤트 정보로 가득하다.
+            // event.target.value : 사용자가 입력한 현재 값
+            this.name = event.target.value + ' ' + lastName;
+        },
+
         add(num) {
             this.counter += num;
         },
