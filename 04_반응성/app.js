649b5df49e8ebb6d8ae7af7206546aeec3f24e5a
Shin Dain <codedain@gmail.com>
2023-06-19 20:49:49 +0900

    03_10_연산 프로퍼티 Computed

--- 04_반응성/app.js
+++ 04_반응성/app.js
@@ -5,14 +5,21 @@ const app = Vue.createApp({
             name: '',
         };
     },
+    computed: {
+        // computed에서 정의한 메서드는 일반 메서드와 다른 방식으로 호출되고 실행
+        // outputFullname이라고 하지 않는 이유는 메서드가 아니라 데이터 프로퍼티처럼 사용되기 때문 (기술적으로는 메서드)
+        // 메서드처럼 사용하지 않고 호출하지도 x
+        fullname() {
+            // 앱이 처음 화면에 렌더링될 때를 제외하곤 삭제하고 카운터를 변경해도 표시되지 x
+            console.log('Running again');
+            if (this.name === '') return '';
+            else return this.name + ' ' + '입니다.';
+        },
+    },
     methods: {
-        // 이름외 다른 내용을 추가하기 위한 메서드
-        // 여기서 문제는 카운트 버튼을 누르면 vue가 이 메서드도 재실행시킨다는 것
-        // vue는 메서드가 하는 일을 모르기 때문에 counter가 사용되는 지 여부는 모르기때문에
-        // 냅다 이 메서드도 실행시키는 것
-        // 이러한 이유로 vue는 이벤트에 바인딩 되지 않은 html 코드 내에 있는 모든 메서드를 재실행시킴
-        // 비록 이 메서드가 counter를 사용하지 않는 다는 것을 내가 알지라도
-        // 이는 성능면에서 좋지 않다.
+        // 이를 위한 해결방법 : 연산(computed) 프로퍼티
+        // 연산 프로퍼티는 메서드와 다른 점은
+        // vue가 의존성을 인식하고 의존성 중 하나가 변경된 경우에만 재실행
         outputFullname() {
             console.log('Running again');
             if (this.name === '') return '';
