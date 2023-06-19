b17d45969138a7c0d254a9df52682abb6fbe821b
Shin Dain <codedain@gmail.com>
2023-06-19 22:25:53 +0900

    03_11_감시자 Watcher
    watch: {}

--- 04_반응성/app.js
+++ 04_반응성/app.js
@@ -3,23 +3,56 @@ const app = Vue.createApp({
         return {
             counter: 0,
             name: '',
+            fullname: '',
+            lastName: '',
         };
     },
-    computed: {
-        // computed에서 정의한 메서드는 일반 메서드와 다른 방식으로 호출되고 실행
-        // outputFullname이라고 하지 않는 이유는 메서드가 아니라 데이터 프로퍼티처럼 사용되기 때문 (기술적으로는 메서드)
-        // 메서드처럼 사용하지 않고 호출하지도 x
-        fullname() {
-            // 앱이 처음 화면에 렌더링될 때를 제외하곤 삭제하고 카운터를 변경해도 표시되지 x
-            console.log('Running again');
-            if (this.name === '') return '';
-            else return this.name + ' ' + '입니다.';
+    watch: {
+        // 감시자 메서드명은 데이터 프로퍼티나 연산 프로퍼티에서 사용한 이름을 사용할 수 있다.
+        // 데이터 프로퍼티의 name
+        // 데이터 프로퍼티의 name이 변경될 때마다 감시자 메서드 vue에 의해 재실행
+        // 즉, name프로퍼티 감시
+        name(value) {
+            // return되는 값은 없다.
+            // this.fullname = this.name + ' ' + 입니다.
+            // 여기서 this.name를 굳이 참조할 필요가 없는 게
+            // 감시자 함수는 watch프로퍼티의 마지막 값을 인수로 가져온다.
+            if (value === '') this.fullname = '';
+            else this.fullname = value + ' ' + this.lastName;
+
+            // value외에 두번째 인수를 받아들일 수도 있다.
+            // 두번째 인수는 watch프로퍼티의 이전 값
+            // 즉, name(newValue, oldValue)
+            // 이제 name이 변경될 때마다 fullname도 업데이트
+        },
+        lastName(value) {
+            if (value === '') this.fullname = '';
+            else this.fullname = this.name + ' ' + value;
+        },
+
+        counter(value) {
+            if (value > 50) {
+                // 데이터 프로퍼티를 업데이트하지만
+                // 항상 업데이트해선 안 되는 로직을 실행하려는 경우
+                // 가령 50을 초과하는 경우에만 카운터를 0으로 설정하려고 한다면
+                // 연산 프로퍼티보다 watch사용
+                this.counter = 0;
+            }
         },
     },
+
+    //연산 프로퍼티도 두 개의 의존성을 사용하도록 수정
+    // 두 개의 의존성을 참조
+    // watch 주석하고 test 꼭 해보기
+    // 원하는 대로 작동하고 watch보다 코드가 많지 x
+    computed: {
+        // fullname() {
+        //     console.log('Running again');
+        //     if (this.name === '' || this.lastName === '') return '';
+        //     else return this.name + ' ' + this.lastName;
+        // },
+    },
     methods: {
-        // 이를 위한 해결방법 : 연산(computed) 프로퍼티
-        // 연산 프로퍼티는 메서드와 다른 점은
-        // vue가 의존성을 인식하고 의존성 중 하나가 변경된 경우에만 재실행
         outputFullname() {
             console.log('Running again');
             if (this.name === '') return '';
