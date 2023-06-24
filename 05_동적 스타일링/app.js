e8d4f599f1ca91189cdf34adb38b0c8a250b60db
Shin Dain <codedain@gmail.com>
2023-06-24 14:52:49 +0900

    05_02_클래스 및 연산(Computed) 프로퍼티

--- 05_동적 스타일링/app.js
+++ 05_동적 스타일링/app.js
@@ -6,6 +6,12 @@ const app = Vue.createApp({
             boxCSelected: false,
         };
     },
+    // 복잡한 동적 클래스 코드가 많으면 특히 유용, 복잡하지 않으면 html에 적어도 ㄱㅊ
+    computed: {
+        boxAClasses() {
+            return { active: this.boxASelected };
+        },
+    },
     methods: {
         // box : 입력값으로 어떤 박스가 선택되었는지 나타내는 식별자
         // toggle 될 수 있게
